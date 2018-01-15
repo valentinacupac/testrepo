bfb1459ac8094c04aebc097e93f3ef4227dc22ae
Valentina Cupac <valentina.cupac@optivem.com>
2018-01-15 21:02:21 +0100

    Created classes

---
+++ TestProject2/src/Main.java
@@ -0,0 +1,48 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		System.out.println("Hello Apple!");
+		
+		System.out.println(5);
+		
+		System.out.println(4 + 3);
+		
+		// PRIMITIVE TYPES
+		
+		
+		// Integer (int)
+		int ab = 14;
+		int lala = 150;
+		int zebra = 234;
+		int luk = 45;
+		
+		// Character (char)
+		char jkl = 'a';
+		char gh = '8';
+		char sfgs = '$';
+		
+		// Boolean (bool)
+		boolean yt = true;
+		boolean lop = false;
+		boolean df = true;
+		
+		// Double 
+		
+		double ujl = 45.89;
+		double er = 786579.982;
+		double vb = 0.06864;
+		
+		
+		// COMPLEX TYPES
+		
+		// String (text - niz karaktera)
+		String jds = "Marija Marjanovic";
+		String ywe = "Ulica 5, Beograd";
+		String ulo = "Fly to the moon";
+	}
+
+}
